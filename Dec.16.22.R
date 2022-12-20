58231d9a66b4aeff2089ef0019f7f877283cd130
slee <sanlee8495@gmail.com>
2022-12-20 11:57:00 -0800

    12/16/22

--- Dec.16.22.R
+++ Dec.16.22.R
@@ -12,29 +12,48 @@ library(lubridate)
 
 file <- readxl::read_excel("622-Torlake Sales Data  for R.xlsx")
 
+
+# Pivoting
 file %>% 
   data.frame() %>% 
   janitor::clean_names() %>% 
-  dplyr::group_by(order_number, or_ty, original_or_num, last_status, next_status, request_date, original_promised, original_promised_time,
-                  actual_ship, x2nd_item_number, description_1, uom, location, lot_serial_number, invoice_number,
-                  customer_po, order_co, hd_cd, ship_to, sold_to, sold_to_name, secondary_quantity, secondary_uo_m, extended_amount,
-                  requested_time, original_or_type, orig_or_co, x3rd_item_number, shipment_number, pick_number, delivery_number,
-                  unit_price, price_uom, foreign_unit_price, foreign_extended_amount, order_date, short_item_no, document_number,
-                  doc_ty, doc_co, scheduled_pick, scheduled_pick_time, actual_ship_time, invoice_date, cancel_date, g_l_date, promised_delivery,
-                  promised_delivery_time, branch_plant, ln_ty, rel_ord, rel_ord_type, related_po_wo_no, related_po_wo_line_no,
-                  transaction_originator, carrier_number, f_h, mod_trn, zone_no, deliver_to, pull_signal) %>% 
+  dplyr::group_by(order_number, or_ty, original_or_num, actual_ship, x2nd_item_number, description_1, uom, customer_po,
+                  ship_to, sold_to, sold_to_name, branch_plant) %>%
+  
   dplyr::summarise(quantity = sum(quantity),
                    quantity_ordered = sum(quantity_ordered),
-                   quantity_shipped = sum(quantity_shipped),
-                   quantity_canceled = sum(quantity_canceled),
-                   quantity_backordered = sum(quantity_backordered)) %>% 
+                   quantity_shipped = sum(quantity_shipped)) %>% 
+  
   dplyr::relocate(quantity, .after = description_1) %>%
   dplyr::relocate(quantity_ordered, .after = uom) %>% 
   dplyr::relocate(quantity_shipped, .after = quantity_ordered) %>% 
-  dplyr::relocate(quantity_canceled, .after = quantity_shipped) %>% 
-  dplyr::relocate(quantity_backordered, .after = quantity_canceled) %>% 
+
   dplyr::ungroup() %>% 
+  dplyr::mutate(actual_ship = as.Date(actual_ship)) %>% 
   data.frame() -> file_2
 
+# Relocating
+file_2 %>% 
+  dplyr::relocate(order_number,	or_ty,	original_or_num,	actual_ship,	x2nd_item_number,	description_1,	quantity,
+                  uom, quantity_ordered, quantity_shipped, customer_po, ship_to,	sold_to,	sold_to_name, branch_plant) -> file_2
+
+
+# Renaming
+colnames(file_2)[1] <- "Order Number"
+colnames(file_2)[2] <- "Or Ty"
+colnames(file_2)[3] <- "Original Or Num"
+colnames(file_2)[4] <- "Actual Ship"
+colnames(file_2)[5] <- "2nd Item Number"
+colnames(file_2)[6] <- "Description 1"
+colnames(file_2)[7] <- "Quantity"
+colnames(file_2)[8] <- "UOM"
+colnames(file_2)[9] <- "Quantity Ordered"
+colnames(file_2)[10] <- "Quantity Shipped"
+colnames(file_2)[11] <- "Customer PO"
+colnames(file_2)[12] <- "Ship To"
+colnames(file_2)[13] <- "Sold To"
+colnames(file_2)[14] <- "Sold To Name"
+colnames(file_2)[15] <- "Branch/Plant"
+  
 
 writexl::write_xlsx(file_2, "Sales Data.xlsx")
